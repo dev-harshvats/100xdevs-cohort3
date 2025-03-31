43079f610d518cc84f68de1c04c7c889c85f4c16
Harsh Vars <h.harshvats@gmail.com>
2025-03-31 17:53:51 +0530

    second commit

---
+++ Week-7.1-MongoDB/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const { UserModel, TodoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "asdasdasd";
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://hharshvats:HarshMongodb@cluster0.hoz54dh.mongodb.net/todo-app-database");
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    })
+
+    res.json({
+        message: "You are logged in"
+    })
+});
+
+app.post("/signin", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+    console.log(user);
+
+    if(user){
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_SECRET)
+        res.json({
+            token: token
+        })
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+});
+
+app.post("/todo", auth, function(req, res){
+    const userId = req.userId;
+
+    res.json({
+        userId: userId
+    })
+});
+
+app.get("/todos", auth, function(req, res){
+    const userId = req.userId;
+
+    res.json({
+        userId: userId
+    })
+});
+
+function auth(req, res, next){
+    const token = req.headers.token;
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if(decodedData){
+        req.userId = decodedData.id;
+        next();
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+}
+
+app.listen(3000, () => {
+    console.log("Server is running on port http://localhost:3000");
+})
